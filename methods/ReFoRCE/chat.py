c1b4565539ce0c00b81f7dbb5965a3c3b0f513dc
Bruce <m2deng@ucsd.edu>
2025-06-30 20:47:24 +0000

    Support client.responses

--- methods/ReFoRCE/chat.py
+++ methods/ReFoRCE/chat.py
@@ -29,6 +29,12 @@ class GPTChat:
                     api_key=os.environ.get("AZURE_OPENAI_KEY"),
                     api_version="2024-12-01-preview"
                 )
+            elif model in ["o3-pro"]:
+                self.client = AzureOpenAI(
+                    azure_endpoint = os.environ.get("AZURE_ENDPOINT"),
+                    api_key=os.environ.get("AZURE_OPENAI_KEY"),
+                    api_version="2025-03-01-preview"
+                )             
             else:
                 self.client = AzureOpenAI(
                     azure_endpoint = os.environ.get("AZURE_ENDPOINT"),
@@ -40,56 +46,64 @@ class GPTChat:
         self.model = model
         self.temperature = float(temperature)
 
-    def get_model_response(self, prompt, code_format):
+    def get_response(self, prompt) -> str:
         self.messages.append({"role": "user", "content": prompt})
-        sql_query = []
-        max_try = 0
-        while not sql_query and max_try < 3:
-            max_try += 1
-            try:
-                response = self.client.chat.completions.create(
-                    model=self.model,
-                    messages=self.messages,
-                    temperature=self.temperature
-                )
-            except Exception as e:
-                if "Error code" in str(e):
-                    print(e)
-                    sys.exit(0)
-                print(e)
-                return e
-            choices = response.choices
-            if choices:
-                main_content = choices[0].message.content
-                sql_query = extract_all_blocks(main_content, code_format)
-            self.messages.append({"role": "assistant", "content": main_content})
-            if not sql_query:
-                print(f"sql_query: {sql_query}, max_try: {max_try}")
-                self.messages.append({"role": "user", "content": f"Please answer in ```{code_format}``` format with one sql query."})
-                continue
-                
-        return sql_query
-    
-    def get_model_response_txt(self, prompt):
-        self.messages.append({"role": "user", "content": prompt})
-        try:
+        if self.model in ["o3-pro"]:
+            response = self.client.responses.create(
+                model=self.model,
+                input=self.messages,
+                temperature=self.temperature
+            )
+            main_content = response.output_text
+        else:
             response = self.client.chat.completions.create(
                 model=self.model,
                 messages=self.messages,
                 temperature=self.temperature
             )
-        except Exception as e:
-            print(e)
-            return e
-        choices = response.choices
-        if choices:
-            main_content = choices[0].message.content
-
+            main_content = response.choices[0].message.content
         self.messages.append({"role": "assistant", "content": main_content})
         return main_content
 
+    def get_model_response(self, prompt, code_format=None) -> list:
+        code_blocks = []
+        max_try = 3
+        while code_blocks == [] and max_try > 0:
+            max_try -= 1
+            try:
+                response = self.get_response(prompt)
+            except Exception as e:
+                print(f"max_try: {max_try}, exception: {e}")
+                continue
+            code_blocks = extract_all_blocks(response, code_format)
+        if max_try == 0 or code_blocks == []:
+            print(f"get_model_response() exit, max_try: {max_try}, code_blocks: {code_blocks}")
+            sys.exit(0)
+            
+        return code_blocks
+
+    def get_model_response_txt(self, prompt):
+        max_try = 3
+        while max_try > 0:
+            max_try -= 1
+            try:
+                response = self.get_response(prompt)
+            except Exception as e:
+                print(f"max_try: {max_try}, exception: {e}")
+                continue
+            break
+        if max_try == 0:
+            print(f"get_model_response_txt() exit, max_try: {max_try}")
+            sys.exit(0)
+        
+        return response
+
     def get_message_len(self):
-        return sum([len(i['content']) for i in self.messages])
+        return {
+            "prompt_len": sum(len(item["content"]) for item in self.messages if item["role"] == "user"),
+            "response_len": sum(len(item["content"]) for item in self.messages if item["role"] == "assistant"),
+            "num_calls": len(self.messages) // 2
+        }
     
     def init_messages(self):
         self.messages = []
